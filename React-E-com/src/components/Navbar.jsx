5f8df659a5aa55e91ba348055c3d9fb02fa8e819
Ishrz <ishrzz66@gmail.com>
2025-12-02 03:29:33 +0530

    updated code

--- React-E-com/src/components/Navbar.jsx
+++ React-E-com/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="bg-amber-200 text-2xl  w-full flex gap-8 items-center justify-center  px-4 py-4 ">
+    <div className="bg-blue-200 text-2xl  w-full flex gap-8 items-center justify-center  px-4 py-4 ">
     <NavLink to='/' className={({isActive})=>{ return isActive? 'text-red-500':'' }}>Home</NavLink>
     <NavLink to='/products' className={({isActive})=>{ return isActive? 'text-red-500':'' }}>Products</NavLink>
     <NavLink to='/about' className={({isActive})=>{ return isActive? 'text-red-500':'' }}>About Us</NavLink>
--- React-E-com/src/components/Products.jsx
+++ React-E-com/src/components/Products.jsx
@@ -1,34 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Loader from './Loader'
 const Products = () => {
     const [products,setProducts]=useState([])
+    const[loader,setLoader]=useState(false)
 
     let fetchData= async ()=>{
-
+        setLoader(true)
         let response=await axios("https://fakestoreapi.com/products")
         setProducts(response.data)
+        setLoader(false)
     }
 
 
     useEffect(()=>{
         fetchData()
     },[])
+console.log(products)
 
-
+if(loader) return <Loader className='p-30 flex justify-center items-center' />
 
   return (
-    <div className='bg-violet-500 w-full h-full'>
+    <div className='bg-violet-500 grid grid-cols-5 gap-3 w-full h-auto pt-7 p-4 max-[900px]:grid-cols-3 max-[550px]:grid-cols-2 max-[550px]:p-1 max-[550px]:pt-6 max-[300px]:grid-cols-1'>
+        
       {products.map((elem,index)=>{
         return(
-
-            <div>
-         <h3>{elem.title}</h3>
-        <h4>{elem.price}</h4>
-        </div>
+        <ProductCard elem={elem} />
         )
       })}
     </div>
   )
 }
+
+const ProductCard=({elem})=>{
+    return(
+        
+        <div key={elem.id} className='card bg-gray-800 text-white flex flex-col gap-3 p-2.5 rounded-2xl items-center justify-center'>
+            
+            <img className='image p-2 aspect-square object-contain' src={elem.image}/>
+            <div className='p-2 '>
+            <h1 className='text-xl  line-clamp-2'>{elem.title}</h1>
+            </div>
+            {/* rating block */}
+            <div className=' self-start para flex gap-3'>
+            <p className='bg-green-600 px-2 py-0.8 rounded-3xl flex items-center justify-between place-items-center text-sm '>⭐{elem.rating.rate}</p>
+            <p >{elem.rating.count}</p>
+            </div>
+             <p className='self-start text-lg text-white/70'>{elem.price} RS</p>
+            
+        </div>
+    )
+}
+
 
 export default Products
