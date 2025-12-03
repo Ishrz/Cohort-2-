992656a0fffd295f915980046739b6324bde5ee3
Ishrz <ishrzz66@gmail.com>
2025-12-04 00:40:11 +0530

    included navigation logic via keyboard key & snake direction logic

--- JS-Logic-Building/Basilisk/script.js
+++ JS-Logic-Building/Basilisk/script.js
@@ -1,15 +1,72 @@
 const board=document.querySelector('.board');
 
-const widhtBlock=30;
-const heightBlock=30;
+const widhtBlock=35;
+const heightBlock=35;
 
 const cols= Math.floor(board.clientWidth /widhtBlock)
 const rows= Math.floor(board.clientHeight / heightBlock)
 
 
+//similar done with below loops
+// for(i=0; i< rows*cols;i++){
+//     let blockdiv=document.createElement('div')
+// blockdiv.classList.add('block')
+//     board.appendChild(blockdiv)
+// }
 
-for(i=0; i< rows*cols;i++){
-    let blockdiv=document.createElement('div')
-blockdiv.classList.add('block')
-    board.appendChild(blockdiv)
+const blocksDiv=[];
+const snake=[
+    {x:5, y:7},
+
+];
+let direction='right'
+
+for (let row=0; row<rows; row++){
+    for( let col=0; col<cols; col++){
+        let blockdiv=document.createElement('div')
+        blockdiv.classList.add('block')
+        board.appendChild(blockdiv)
+        blockdiv.innerText=`${row}-${col}`
+        blocksDiv[`${row}-${col}`] = blockdiv;
+    }
+
 }
+
+const render=()=>{
+     snake.forEach((segment)=>{
+        blocksDiv[`${segment.x}-${segment.y}`].classList.add('fill')
+    })
+}
+
+setInterval(()=>{
+    let head=null;
+
+    if(direction==='left'){
+        head={ x:snake[0].x ,y:snake[0].y-1}
+    }else if(direction==='right'){
+        head={x:snake[0].x, y:snake[0].y+1}
+    }else if(direction==='down'){
+        head={x:snake[0].x+1, y:snake[0].y}
+    }else if(direction==='up'){
+        head={x:snake[0].x-1, y:snake[0].y}
+    }
+
+    snake.forEach(segment=>{
+        blocksDiv[`${segment.x}-${segment.y}`].classList.remove('fill')
+    })
+    
+    
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+},1000)
+
+
+
+addEventListener('keydown',(evt)=>{
+    if(evt.key=== 'ArrowUp')  direction='up'
+    if(evt.key=== 'ArrowDown')  direction='down'
+    if(evt.key=== 'ArrowRight')  direction='right'
+    if(evt.key=== 'ArrowLeft')  direction='left'
+})
