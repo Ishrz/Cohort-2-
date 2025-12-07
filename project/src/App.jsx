cf2c27d836e509636692fa3ff8feb75444f30f97
Ishrz <ishrzz66@gmail.com>
2025-12-07 19:40:06 +0530

    Hero Section Done

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <div className="mx-w-7xl mx-auto pt-20 px-6">
-
+      <div className="max-w-7xl mx-auto pt-20 px-6">
       <HeroSection/>
+      <FeatureSection/>
       </div>
+      
     </div>
   )
 }
